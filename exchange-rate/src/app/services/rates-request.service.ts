34760c690b8d7fb2d99d77e2a2b5225c17081a60
Liliia Paliichuk <113315152+lili2628@users.noreply.github.com>
2023-11-10 12:56:17 +0200

    added styles

--- exchange-rate/src/app/services/rates-request.service.ts
+++ exchange-rate/src/app/services/rates-request.service.ts
@@ -13,7 +13,7 @@ export class RatesRequestService {
   constructor(private http: HttpClient) {}
 
   getRates(): Observable<Currency> {
-      return this.http.get<Currency>(`${this.apiUrl}/v3/latest?apikey=cur_live_A71r5EE7KNPps1K7HMAVkceamqgPLWMqRZD6HVzA&base_currency=UAH`);
+     return this.http.get<Currency>(`${this.apiUrl}/v3/latest?apikey=cur_live_A71r5EE7KNPps1K7HMAVkceamqgPLWMqRZD6HVzA&base_currency=UAH`);
   }
 
 }
